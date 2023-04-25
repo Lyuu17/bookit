34d6ebdd37be0909057dbc80a7a4006361a3a65d
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-04-25 21:03:13 +0200

    Implementation of the website's colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",],
   theme: {
+    colors: {
+      backgroundBookIt: '#222028',
+      fontColor: '#a5afac',
+      secondfontColor: '#a5747e'
+    },
     extend: {},
   },
   plugins: [],
