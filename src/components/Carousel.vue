0c4f0f2daa81122c2360f4f70f9e696c64812f38
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-05-19 18:07:23 +0200

    carousel images implemented in imgs folder

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,14 +2,14 @@
   <section class="marcas">
     <div class="swiper-container marca-slider">
       <div class="swiper-wrapper">
-        <div class="swiper-slide"><img src="../img/1.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/2.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/3.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/4.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/5.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/6.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/7.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../img/8.png" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/1.png" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/2.jpg" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/3.jpg" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/4.jpg" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/5.png" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/6.png" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/7.jpg" alt="" /></div>
+        <div class="swiper-slide"><img src="../imgs/8.png" alt="" /></div>
       </div>
     </div>
   </section>
