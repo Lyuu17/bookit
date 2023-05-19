20228eb82f30c4ccff8118a41156e4a0f71b486a
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-05-19 17:50:08 +0200

    Footer component implemented

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@
   <body>
     <router-view></router-view>
   </body>
+  <Footer></Footer>
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import Home from "./components/Home.vue";
 import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style scoped></style>
--- src/components/TheWelcome.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div>
-    <h1>Aquí ira el resto de cosas como las cartas de hotel</h1>
-  </div>
-  
-</template>
