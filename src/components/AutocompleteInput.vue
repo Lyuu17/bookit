1a7c3fee1d8de67f2aa0feb21c25b4b9211a94d3
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-05-30 22:51:57 +0200

    Design of AutocompleteInput completed

---
+++ src/components/AutocompleteInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input
+      v-model="searchText"
+      @input="search"
+      type="text"
+      placeholder="Buscar provincia"
+    />
+    <ul v-if="showSuggestions">
+      <li
+        v-for="suggestion in suggestions"
+        :key="suggestion"
+        @click="selectSuggestion(suggestion)"
+      >
+        {{ suggestion }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+</script>
