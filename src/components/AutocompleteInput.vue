cb54a88635bedb43008fcc4265fb924c31d036ed
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-05-30 22:53:09 +0200

    Methods implemented in AutocompletedInput

--- src/components/AutocompleteInput.vue
+++ src/components/AutocompleteInput.vue
@@ -20,4 +20,76 @@
 
 <script>
 export default {
+  data() {
+    return {
+      searchText: "",
+      suggestions: [],
+      provinces: [
+        "Álava",
+        "Albacete",
+        "Alicante",
+        "Almería",
+        "Asturias",
+        "Ávila",
+        "Badajoz",
+        "Barcelona",
+        "Burgos",
+        "Cáceres",
+        "Cádiz",
+        "Cantabria",
+        "Castellón",
+        "Ciudad Real",
+        "Córdoba",
+        "Cuenca",
+        "Gerona",
+        "Granada",
+        "Guadalajara",
+        "Guipúzcoa",
+        "Huelva",
+        "Huesca",
+        "Islas Baleares",
+        "Jaén",
+        "La Coruña",
+        "La Rioja",
+        "Las Palmas",
+        "León",
+        "Lérida",
+        "Lugo",
+        "Madrid",
+        "Málaga",
+        "Murcia",
+        "Navarra",
+        "Orense",
+        "Palencia",
+        "Pontevedra",
+        "Salamanca",
+        "Santa Cruz de Tenerife",
+        "Segovia",
+        "Sevilla",
+        "Soria",
+        "Tarragona",
+        "Teruel",
+        "Toledo",
+        "Valencia",
+        "Valladolid",
+        "Vizcaya",
+        "Zamora",
+        "Zaragoza",
+      ],
+      showSuggestions: false,
+    };
+  },
+  methods: {
+    search() {
+      this.suggestions = this.provinces.filter((province) =>
+        province.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+      this.showSuggestions = true;
+    },
+    selectSuggestion(suggestion) {
+      this.searchText = suggestion;
+      this.showSuggestions = false;
+    },
+  },
+};
 </script>
