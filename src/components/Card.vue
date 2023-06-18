87768621d2c36def460b2e63d24daf0a68c7bcb9
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-06-19 01:32:02 +0200

    Receiving data by props from Home

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,7 +16,6 @@
           fill="currentColor"
           viewBox="0 0 20 20"
         >
-          <title>First star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -28,7 +27,6 @@
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Second star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -40,7 +38,6 @@
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Third star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -52,7 +49,6 @@
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Fourth star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -64,7 +60,6 @@
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Fifth star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -76,16 +71,27 @@
       </div>
       <div class="flex items-center justify-between">
         <span class="text-3xl font-bold text-gray-900">$599</span>
-        <a
-          href="#"
-          class="text-white focus:ring-4 bg-green-500 py-3 focus:outline-none font-medium rounded-lg text-base px-5 text-center"
-          >Book</a
-        >
+        <input
+          type="button"
+          class="text-white focus:ring-4 bg-green-500 py-2 focus:outline-none font-medium rounded-lg text-base px-5 text-center"
+          value="Book it"
+        />
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    hotels: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
 
 <style scoped></style>
