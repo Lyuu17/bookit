331c61aab4ecc74f263ef0eb388ee82458259d21
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-06-19 01:34:47 +0200

    Collaborator image sample with transitions

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,42 +1,70 @@
 <template>
   <section class="brands">
-    <div class="swiper-container brand-slider">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide"><img src="../assets/imgs/1.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/2.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/3.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/4.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/5.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/6.png" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/7.jpg" alt="" /></div>
-        <div class="swiper-slide"><img src="../assets/imgs/8.png" alt="" /></div>
+    <div class="w-full overflow-hidden">
+      <h1
+        class="text-center py-7 text-4xl font-bold bg-gradient-to-r from-green-700 to-green-300 bg-clip-text text-transparent bg-green-700 transition-all duration-10000"
+      >
+        Our Collaborators
+      </h1>
+      <div class="flex">
+        <img
+          src="../assets/imgs/1.png"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 === 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/2.jpg"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 !== 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/3.jpg"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 === 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/4.jpg"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 !== 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/5.png"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 === 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/6.png"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 !== 0 ? 1 : 0 }"
+        />
+        <img
+          src="../assets/imgs/7.jpg"
+          alt=""
+          class="w-1/6 h-auto object-cover transition-opacity duration-15000"
+          :style="{ opacity: currentIndex % 2 === 0 ? 1 : 0 }"
+        />
       </div>
     </div>
   </section>
 </template>
 
-<script setup>
-/*import Swiper from "swiper";
-
-var swiper = new Swiper(".marca-slider", {
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableonInteraction: false,
+<script lang="ts">
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+    };
   },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
+  mounted() {
+    setInterval(() => {
+      this.currentIndex += 1;
+    }, 2500);
   },
-});*/
+};
 </script>
-
-<style scoped></style>
