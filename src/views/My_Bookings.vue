16d77922873b66b831180762597f5f968956bccd
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-06-20 23:38:21 +0200

    attempt to get the itineraries

--- src/views/My_Bookings.vue
+++ src/views/My_Bookings.vue
@@ -3,26 +3,34 @@
 </template>
 
 <script lang="ts">
+import { defineComponent, onBeforeMount } from "vue";
 import { useStore } from "pinia";
 import axios from "axios";
 
-export default {
-  beforeMount() {
+export default defineComponent({
+  setup() {
     const store = useStore();
-    this.makeRequest(store.setResponseData);
-  },
-  methods: {
-    async makeRequest(setResponseData) {
+    const token = store.responseData;
+
+    onBeforeMount(() => {
+      makeRequest(token);
+    });
+
+    async function makeRequest(token) {
       try {
-        const headers = { Authorization: `Bearer ${setResponseData}` }; // auth header with bearer token
+        const headers = { Authorization: `Bearer ${token}` };
         const response = await axios.get("/api/v1/itineraries", { headers });
         console.log(response.data);
       } catch (error) {
         console.error(error);
       }
-    },
+    }
+
+    return {
+      makeRequest,
+    };
   },
-};
+});
 </script>
 
 <style scoped></style>
