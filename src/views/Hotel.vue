7d3369d5ad43b792acaeda3460cfe526342988e8
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-06-20 15:28:00 +0200

    Image and CardRoom component implemented

--- src/views/Hotel.vue
+++ src/views/Hotel.vue
@@ -1,53 +1,70 @@
 <template>
-  <section class="py-5 bg-indigo-200 text-white text-center">
-    <div class="container mx-auto flex items-center">
-      <div class="w-full">
-        <h2 class="text-4xl font-nunito text-black">{{ hotel.name }}</h2>
+  <div>
+    <section class="py-5 bg-gray-100 text-white text-center">
+      <div class="relative w-screen h-96">
+        <img
+          src="https://images.pexels.com/photos/2869215/pexels-photo-2869215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Imagen del hotel"
+          class="w-full h-full object-cover backdrop-filter backdrop-blur-sm brightness-75"
+        />
+        <div
+          class="absolute inset-0 flex flex-col items-center justify-center text-white"
+        >
+          <h3 class="text-xl py-2">Bienvenido/a a</h3>
+          <h2 class="text-5xl font-normal">{{ hotel.name }}</h2>
+        </div>
       </div>
-    </div>
-  </section>
-  <section class="bg-gray-100 py-5">
-    <div class="container mx-auto grid grid-cols-2 gap-4">
-      <div class="mt-8 flex flex-col items-center border-r-4">
-        <h2 class="text-2xl font-bold">Contact Details</h2>
+    </section>
+    <section class="bg-gray-100 pb-5">
+      <div class="container mx-auto grid grid-cols-2 gap-4">
+        <div class="mt-8 flex flex-col items-center border-r-4">
+          <h2 class="text-2xl pb-5 font-bold">Contact Details</h2>
+          <p class="text-lg">
+            Address: {{ hotel.address.street }}, {{ hotel.address.country }}
+          </p>
+          <p class="text-lg">Postal code: {{ hotel.address.postal_code }}</p>
+          <p class="text-lg">Telephone: {{ hotel.phone }}</p>
+        </div>
+        <div class="mt-8 flex flex-col items-center">
+          <h2 class="text-2xl pb-5 font-bold">Servicios</h2>
+          <ul>
+            <li v-for="hotelItem in hotel.amenities" class="text-center">
+              {{ hotelItem.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="mt-8 text-center">
+        <h2 class="text-2xl font-bold">Check-in</h2>
         <p class="text-lg">
-          Address: {{ hotel.address.street }}, {{ hotel.address.country }}
+          Check-in hours: {{ hotel.checkin.begin_time }} - {{ hotel.checkin.end_time }}
         </p>
-        <p class="text-lg">Postal code: {{ hotel.address.postal_code }}</p>
-        <p class="text-lg">Telephone: {{ hotel.phone }}</p>
+        <p class="text-lg">Minimun age: {{ hotel.checkin.min_age }}</p>
       </div>
-      <div class="mt-8 flex flex-col items-center">
-        <h2 class="text-2xl font-bold">Servicios</h2>
-        <ul>
-          <li v-for="hotelItem in hotel.amenities" class="text-center">
-            {{ hotelItem.name }}
-          </li>
-        </ul>
+    </section>
+    <section class="mt-8 text-center">
+      <h2 class="text-3xl font-bold py-5">Our rooms</h2>
+      <div class="flex justify-center">
+        <div v-for="room in hotel.rooms" class="mx-2">
+          <CardRoom />
+        </div>
       </div>
-    </div>
-  </section>
-  <section>
-    <div class="mt-8 text-center">
-      <h2 class="text-2xl font-bold">Check-in</h2>
-      <p class="text-lg">
-        Check-in hours: {{ hotel.checkin.begin_time }} - {{ hotel.checkin.end_time }}
-      </p>
-      <p class="text-lg">Minimun age: {{ hotel.checkin.min_age }}</p>
-    </div>
-  </section>
-  <section>
-    <p class="text-lg">Check-in instructions: {{ hotel.checkin.instructions }}</p>
-  </section>
-  <Testimonies />
+    </section>
+    <Testimonies />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from "vue";
 import Testimonies from "../components/Testimonies.vue";
+import CardRoom from "../components/CardRoom.vue";
 
 export default defineComponent({
   components: {
     Testimonies,
+    CardRoom,
   },
   computed: {
     hotel() {
