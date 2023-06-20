facc4fec84d187912539003441c44ad4ecd1feb2
LuciaRodriguez <lucia.lmrr23@gmail.com>
2023-06-21 00:32:54 +0200

    Change design

--- src/components/CardBooking.vue
+++ src/components/CardBooking.vue
@@ -3,7 +3,10 @@
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{{ hotelName }} Hotel name</div>
       <div class="mb-2">
-        <span class="font-semibold">Check-in:</span> {{ checkinDate }}
+        <span class="font-semibold"
+          >Check-in:
+          <p class="font-normal">{{ checkinDate }} checkin</p></span
+        >
       </div>
       <div class="mb-2">
         <span class="font-semibold">Check-out:</span> {{ checkoutDate }}
--- src/views/MyBookingsView.vue
+++ src/views/MyBookingsView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <CardBooking />
+  <div class="bg-blue-100">
+    <section class="bg-gray-100 text-white text-center">
+      <div class="relative w-screen h-96">
+        <img
+          src="https://images.pexels.com/photos/16253102/pexels-photo-16253102/free-photo-of-escaleras-hotel-ventanas-puerta.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Booking image"
+          class="w-full h-full border-5 object-cover backdrop-filter backdrop-blur-sm brightness-75"
+        />
+      </div>
+      <div class="absolute inset-0 flex flex-col items-center justify-center text-white">
+        <h2 class="text-5xl font-normal"></h2>
+      </div>
+    </section>
+    <section class="py-5">
+      <CardBooking />
+    </section>
   </div>
 </template>
 
